0d6b8f5e8281a3cf1ed0cb7be37851e6f7653cc7
bwestbrook <jamin.westbrook@gmail.com>
2025-01-10 17:53:14 -0800

    heroku config updates

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm start
+server: node server.js
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 3000,
+    hot: true, // Enable Hot Module Replacement
+    liveReload: true, // Enable live reloading
+    allowedHosts: 'all'
+  },
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
     optimization: {
@@ -9,5 +15,5 @@ module.exports = defineConfig({
         chunks: "all",
       },
     },
-  },
+  }
 });
